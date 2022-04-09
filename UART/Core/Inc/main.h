44eff9f4439cc2c9e9aeb2cc6964f29a16cbfc1e
Teruru-52 <83484632+Teruru-52@users.noreply.github.com>
2022-04-09 18:55:03 +0900

    delete #include <stdint.h>

--- UART/Core/Inc/main.h
+++ UART/Core/Inc/main.h
@@ -33,7 +33,6 @@ extern "C" {
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include <stdio.h>
-#include <stdint.h>
 /* USER CODE END Includes */
 
 /* Exported types ------------------------------------------------------------*/
